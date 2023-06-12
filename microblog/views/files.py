9c948aef6ea6729bbdbadb6280cba5799658c9bf
Mustafa Baser <mustafa.baser@stu.omu.edu.tr>
2023-06-12 12:38:18 +0300

    dosya indirme ve silme

--- microblog/views/files.py
+++ microblog/views/files.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
-from flask import Blueprint, render_template, request, current_app, flash
+from flask import Blueprint, render_template, request, current_app, flash,\
+    send_from_directory, redirect, url_for
 from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
 
@@ -27,3 +28,22 @@ def index():
     user_files_path = Path(current_user.upload_dir)
 
     return render_template('files/index.html', title=title, form=form, user_files_path=user_files_path)
+
+@files_bp.route('/download/<filename>')
+@login_required
+def download(filename):
+    if os.path.exists(os.path.join(current_user.upload_dir, filename)):
+        return send_from_directory(current_user.upload_dir, filename)
+    else:
+        flash("Böyle bir dosya yok")
+        return redirect(url_for('files.index'))
+
+@files_bp.route('/delete/<filename>')
+@login_required
+def delete(filename):
+    file_path = os.path.join(current_user.upload_dir, filename)
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    else:
+        flash("Böyle bir dosya yok")
+    return redirect(url_for('files.index'))
