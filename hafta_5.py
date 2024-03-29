bf1bbb70760c9dd992d1076cb2172ef2e2e081fe
Mustafa Baser <mustafa.baser@stu.omu.edu.tr>
2024-03-29 14:17:21 +0300

    temp1

--- hafta_5.py
+++ hafta_5.py
@@ -13,54 +13,9 @@ def saat():
 
 @app.route('/yemek-menusu')
 def yemek_menusu():
-    return """
+    html_metin = open('yemek_menusu.html').read()
+    return html_metin
     
-    <html>
-    <head>
-    <title>Günün Menüsü</title>
-    </head>
-    <body>
-    <table border="1">
-    <tr><th>Kahvaltı</th><th>Öğle Yemeği</th><th>Akşam Yemeği</th>
-    <tr>
-        <td>
-            <ul>
-                <li> Peynir
-                <li> Zeytin
-                <li> Yumurta
-                <li> Çay
-            </ul>
-        </td>
-        
-        <td>
-            <ul>
-                <li> Çorba
-                <li> Kuru fasulye
-                <li> Pilav
-                <li> Turşu
-            </ul>
-        </td>
-
-        <td>
-            <ul>
-                <li> Salata
-                <li> Makarna
-                <li> Kadayıf
-            </ul>
-        </td>
-
-    </tr>
-    
-    </table>
-    </body>
-    </html>
-    
-    
-    
-    
-    """
-
-
 
 
 app.run(debug=True)
