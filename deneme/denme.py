2fb3cc4a16424bb941500a45c417553ce3fccb5a
Mustafa Baser <mustafa.baser@stu.omu.edu.tr>
2026-02-20 15:29:04 +0300

    deneme klasoru

---
+++ deneme/denme.py
@@ -0,0 +1,31 @@
+import time
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+    kullanici = {'adi': 'Hüsna', 'soyadi': 'Cömert'}
+    iletiler = [
+        {
+            'yazar': {'adi': 'Yağmur'},
+            'ileti': 'Ah şu MacOS çalışsa, neler yapacağım'
+        },
+        {
+            'yazar': {'adi': 'Hüsna'},
+            'ileti': 'Ben yapıyorum, bilgisayar çalışmıyor'
+        },
+        {
+            'yazar': {'adi': 'Mustafa Hoca'},
+            'ileti': 'Hiç kimseye bişey anlatamıyorum'
+        },
+
+        
+    ]
+    return render_template(
+            'index.html', 
+            kullanici=kullanici,
+            saat='16:07',
+            baslik='Ana Sayfa',
+            iletiler=iletiler
+            )
+app.run(debug=True)
